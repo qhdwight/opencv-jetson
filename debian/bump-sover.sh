396195ea38bc0aa4a7673a785e79f403072e1486
Mo Zhou <cdluminate@gmail.com>
2020-09-03 12:50:18 +0800

    Bump VERSION from 4.3->4.4 for all control files.

--- debian/bump-sover.sh
+++ debian/bump-sover.sh
@@ -1,5 +1,5 @@
-files="$(ls *4.2*)"
+files="$(ls *4.3*)"
 for f in $files; do
-	echo $f -- $(echo $f | sed -e 's/4\.2/4.3/')
-	mv $f $(echo $f | sed -e 's/4\.2/4.3/') -v
+	echo $f -- $(echo $f | sed -e 's/4\.3/4.4/')
+	mv $f $(echo $f | sed -e 's/4\.3/4.4/') -v
 done
