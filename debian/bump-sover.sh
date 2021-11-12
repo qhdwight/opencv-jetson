485bb25067713e58aaaf21444e4debe19ecc8203
Timo Röhling <roehling@debian.org>
2021-11-13 00:23:56 +0100

    Generalize bump-sover.sh helper script
    Execute ./bump-sover.sh <NEW_SOVER> to rename debian/*.install files
    and update debian/control.
    
    Gbp-Dch: ignore

--- debian/bump-sover.sh
+++ debian/bump-sover.sh
@@ -1,5 +1,27 @@
-files="$(ls *4.3*)"
-for f in $files; do
-	echo $f -- $(echo $f | sed -e 's/4\.3/4.4/')
-	mv $f $(echo $f | sed -e 's/4\.3/4.4/') -v
+#!/bin/bash
+set -e
+debian_dir="$(dirname "$0")"
+[ -z "$debian_dir" ] || cd "$debian_dir"
+old_sover="$(grep "^Package: libopencv-core[0-9]" control | cut -c24-)"
+new_sover="${1:?need new soversion as parameter}"
+[[ "$new_sover" = [0-9]* ]] || (echo>&2 "soversion must start with a digit"; exit 1)
+if [[ "$old_sover" = "$new_sover" ]]
+then
+	echo nothing to be done: $old_sover == $new_sover
+	exit 0
+fi
+packages=(
+	$(grep "^Package: lib" control | grep -F "${old_sover}" | cut -c10-)
+)
+for old_pkg in "${packages[@]}"
+do
+	new_pkg="${old_pkg/${old_sover}/${new_sover}}"
+	sed_args+=("-e" "s/${old_pkg/./\\.}/${new_pkg}/")
+	for old_f in ${old_pkg}.*
+	do
+		new_f="${old_f/${old_sover}/${new_sover}}"
+		mv -v "$old_f" "$new_f"
+	done
 done
+sed -i "${sed_args[@]}" control
+
